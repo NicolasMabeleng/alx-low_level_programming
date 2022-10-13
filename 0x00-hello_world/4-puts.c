3591ccfe116691e6df96080d522cd1934467ae44
NicolasMabeleng <mabelengnicolas@outlook.com>
2022-10-13 15:34:23 -0700

    Programming is awesome

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,35 +1,10 @@
-#include <stdio.h>                                                  
-
-                                                                    
-
-/**                                                                 
-
-                                                                    
-
-*main - serves as the entrypoint for the program                    
-
-                                                                    
-
-*Return - by default returns zero if no errors otherwise returns non
-
-                                                                    
-
--zero value                                                         
-
-                                                                    
-
-*/                                                                  
-
-                                                                    
-
-int main(void)                                                      
-
-                                                                    
-
-{                                                                   
-
-                                                                    
-
-puts("Programming is like building a multilingual puzzle\n");
-return (0)
-} 
+#include <stdio.h>
+/**
+*main - serves as the entry point for the program
+*Return - returns zero by default if no errors else returns non-zero value
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
