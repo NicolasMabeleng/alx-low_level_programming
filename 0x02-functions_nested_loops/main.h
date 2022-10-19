926cff629175d952ca46e5becf8d9db2f52b2ac5
NicolasMabeleng <mabelengnicolas@outlook.com>
2022-10-19 16:46:53 -0700

    loops

--- 0x02-functions_nested_loops/main.h
+++ 0x02-functions_nested_loops/main.h
@@ -4,8 +4,8 @@
 void print_alphabet(void);
 void print_alphabet_x10(void);
 int _islower(int c);
-int _isaplha(int c);
-int print_sing(int n);
+int _isalpha(int c);
+int print_sign(int n);
 int _abs(int);
 int print_last_digit(int);
 void jack_bauer(void);
