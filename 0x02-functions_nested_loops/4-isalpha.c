1b6ebe842a2acaa4f5db62c23a50232c355b4758
NicolasMabeleng <mabelengnicolas@outlook.com>
2022-10-19 15:48:30 -0700

    functions, nested loops

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -3,11 +3,11 @@
 /**
  * _isalpha - checks for alphabets
  *@c: character to be checked
- * Return: 1 if character is a letter, 0 otherwise
+ * Return: 1 if lowercase/uppercase, 0 if not
  */
 int _isalpha(int c)
 {
-	if ((c >= 'a' && c <= 'z') || (C >= 'A' && C <= 'Z'))
+	if ((c >= 'a' && c <= 'z') || (c >= 'A' && c <= 'Z'))
 	{
 		return (1);
 	}
