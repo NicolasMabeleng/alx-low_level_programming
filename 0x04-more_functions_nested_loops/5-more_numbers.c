af5e5f291c828848bd250d4d56a7a3c332566915
NicolasMabeleng <Mabelengnicolas@outlook.com>
2022-11-07 04:17:26 -0800

    changes

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -2,22 +2,20 @@
 
 /**
  * more_numbers - prints 0 to 14 ten times
- * Return: Always 0
+ * Return: no return
  */
 
 void more_numbers(void)
 {
 	int num, a;
 
-	for (a = 0; a <= 9; a++)
+	for (a = 0; a <= 10; a++)
 	{
-		for (num = 0; num <= 14; num++)
+		for (num = 0; num <= 15; num++)
 		{
-			if num > 9)
-			{
-				_putchar((num / 10) + '0');
-			}
-			_putchar((num % 10) + '0');
+			if num >= 10)
+				_putchar((num / 10) + 48);
+			_putchar((num % 10) + 48);
 		}
 
 		_putchar('\n');
--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -7,17 +7,18 @@
 
 void print_diagonal(int n)
 {
-	int x, int y;
-
-	if (n <= 0)
-		_putchar('\n');
+	int x, y;
 
 	for (x = 0; x < n; x++)
 	{
 		for (y = 0; y < x; y++)
+		{
 			_putchar(' ');
+		}
+		_putchar(92);
 
-		_putchar('\\');
-		_putchar('\n');
+		if (x < (n - 1))
+			_putchar('\n');
 	}
+	_putchar('\n');
 }
