33390e7def44b64f85af15c902dc475d2efd8918
NicolasMabeleng <Mabelengnicolas@outlook.com>
2022-11-15 14:12:39 -0800

    0x0E-structures_typedef

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,5 @@
-#ifndef DOG_H
-#define DOG_H
+#ifndef _DOG_H_
+#define _DOG_H_
 
 /**
  * dog_t - Typedef for struct dog
@@ -28,5 +28,6 @@ void init_dog(struct dog *d, char *name, float age, char *owner);
 void print_dog(struct dog *d);
 dog_t *new_dog(char *name, float age, char *owner);
 void free_dog(dog_t *d);
+int main(void);
 
 #endif
